25da68f05351e542577d51f4a9603f7437f02daa
ongaeshi <ongaeshi0621@gmail.com>
2015-06-20 00:39:00 +0900

    Add Podfile

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NYXImagesKit
+#define COCOAPODS_POD_AVAILABLE_NYXImagesKit
+#define COCOAPODS_VERSION_MAJOR_NYXImagesKit 2
+#define COCOAPODS_VERSION_MINOR_NYXImagesKit 3
+#define COCOAPODS_VERSION_PATCH_NYXImagesKit 0
+
