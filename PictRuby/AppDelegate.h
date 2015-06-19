0ca3a02aa91a93fa2106dc1be54f7b85975e4f10
ongaeshi <ongaeshi0621@gmail.com>
2015-06-20 00:16:19 +0900

    Initial Commit

---
+++ PictRuby/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictRuby
+//
+//  Created by ongaeshi on 2015/06/20.
+//  Copyright (c) 2015年 ongaeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictRuby/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PictRuby
+//
+//  Created by ongaeshi on 2015/06/20.
+//  Copyright (c) 2015年 ongaeshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
