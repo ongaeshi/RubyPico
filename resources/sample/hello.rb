43c62a4dba992bc2b25dc5d23ef0806c9d3e2602
ongaeshi <ongaeshi0621@gmail.com>
2015-06-22 01:51:20 +0900

    Update sample/hello.rb

--- resources/sample/hello.rb
+++ resources/sample/hello.rb
@@ -1,22 +1,18 @@
-def setup
-  set_background 255, 255, 255
-  @x = 180
-  @y = 0
+# Please return the Image object in the "def convert"
+# LX = 70
+# LY = 60
+
+def convert
+  #img = Image.load("sample.jpg")  
+  img = Image.pick_from_library
+  
+  # img = img.crop(
+  #   LX, LY,
+  #        img.width - LX, img.height - 280
+  #    )
+
+  #image = img.crop(10, 10, img.width - 10, img.height -10)  
+  #img = img.crop(190, 190, 20, 100)
+  
+  img
 end
-
-def update
-  @y += 1
-end
-
-def draw
-  set_color 50, 200, 50
-  circle 160, 200, 100
-
-  set_color 200, 50, 50
-  @x = Input.touch(0).x if Input.touch(0).valid?
-  rect @x, @y, 100, 80
-
-  set_color 0, 0, 0
-  text "Hello world!", 120, 160
-end
-
