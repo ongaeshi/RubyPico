220f22eb6215d0cbe051ad5102a2fe92b440efae
ongaeshi <ongaeshi0621@gmail.com>
2015-09-20 17:05:59 +0900

    Add sample/grid4.rb

---
+++ resources/sample/grid4.rb
@@ -0,0 +1,25 @@
+# Please return the Image object in the "def convert"
+X = 2
+Y = 2
+NUM = X * Y
+
+def convert
+  imgs =  []
+  imgs.push Image.pick_from_library
+  imgs.push Image.pick_from_library
+  imgs.push Image.pick_from_library
+  imgs.push Image.pick_from_library
+
+  BX = imgs[0].width
+  BY = imgs[0].height
+  
+   Image.render(BX*2, BY*2) do 
+     imgs[0].draw(0, 0, BX, BY)
+     imgs[1].draw(BX, 0, BX, BY)
+     imgs[2].draw(0, BY, BX, BY)
+     imgs[3].draw(BX, BY, BX, BY)
+  end
+  
+  #img.save  
+end
+
