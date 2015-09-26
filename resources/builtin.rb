eeb7a0c1829b66816d22c4ddeeb8008d1c2d1677
ongaeshi <ongaeshi0621@gmail.com>
2015-09-26 12:43:13 +0900

    Add ImageUtil module

--- resources/builtin.rb
+++ resources/builtin.rb
@@ -34,3 +34,46 @@ class Image
     end
   end    
 end
+
+module ImageUtil
+  def self.vertical(imgs)
+    width = imgs[0].width
+    height = imgs.reduce(0) { |a, e| a += fith(e, width) }
+
+    Image.render(width, height) do
+      y = 0
+      imgs.each_with_index do |img, i|
+        h = fith(img, width)
+        img.draw(0, y, width, h)
+        y += h
+      end
+    end
+  end
+
+  def self.horizontal(imgs)
+    height = imgs[0].height
+    width  = imgs.reduce(0) { |a, e| a += fitw(e, height) }
+
+    Image.render(width, height) do
+      x = 0
+      imgs.each_with_index do |img, i|
+        w = fitw(img, height)
+        img.draw(x, 0, w, height)
+        x += w
+      end
+    end
+  end
+
+  def self.grid(imgs)
+  end
+
+  private
+
+  def self.fitw(img, height)
+    (img.width.to_f / img.height) * height
+  end
+
+  def self.fith(img, width)
+     (img.height.to_f / img.width) * width
+  end
+end
