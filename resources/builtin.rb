c954979d0d06d774e4e9501c1b1e1f763ad692e9
ongaeshi <ongaeshi0621@gmail.com>
2015-09-27 19:47:09 +0900

    Add ImageUtil.grid

--- resources/builtin.rb
+++ resources/builtin.rb
@@ -65,6 +65,22 @@ module ImageUtil
   end
 
   def self.grid(imgs)
+    grid = grid_size(imgs.length)
+
+    width = imgs[0].width
+    height = imgs[0].height
+
+    ygrid = (imgs.length / grid).to_i 
+    ygrid += 1 if imgs.length % grid > 0
+
+    Image.render(width*grid, height*ygrid) do 
+      (0...grid).each do |y|
+        (0...grid).each do |x|
+          img = imgs[y*grid + x]
+          img.draw(x*width, y*height, width, height) if img
+        end
+      end
+    end
   end
 
   private
@@ -75,5 +91,15 @@ module ImageUtil
 
   def self.fith(img, width)
      (img.height.to_f / img.width) * width
+  end
+
+  def self.grid_size(num)
+    # Math.sqrt
+    array = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    array.each_with_index do |e, i|
+      return i + 1 if num <= e
+    end
+
+    array.length
   end
 end
--- resources/sample/grid.rb
+++ resources/sample/grid.rb
@@ -2,32 +2,5 @@
 
 def convert
   imgs = Image.pick_from_library(81)
-
- GRID = grid_size(imgs.length)
-
-  X = imgs[0].width
-  Y = imgs[0].height
-
-  ygrid = (imgs.length / GRID).to_i 
-  ygrid += 1 if imgs.length % GRID > 0
-
-  Image.render(X*GRID, Y*ygrid) do 
-    (0...GRID).each do |y|
-      (0...GRID).each do |x|
-        img = imgs[y*GRID + x]
-        img.draw(x*X, y*Y, X, Y) if img
-      end
-    end
-  end
+  ImageUtil.grid(imgs)
 end
-
-def grid_size(num)
-  # Math.sqrt
-  array = [1, 4, 9, 16, 25, 36, 49, 64, 81]
-  array.each_with_index do |e, i|
-    return i + 1 if num <= e
-  end
-
-  9
-end
-
