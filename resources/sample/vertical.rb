740a61e991acd7803cb8ab6e161eb0af1c25ea40
ongaeshi <ongaeshi0621@gmail.com>
2015-09-27 19:50:56 +0900

    Rewrite sample using ImageUtil

--- resources/sample/vertical.rb
+++ resources/sample/vertical.rb
@@ -1,23 +1,6 @@
 # Please return the Image object in the "def convert"
 
 def convert
-  imgs = Image.pick_from_library(8)
-
-  WIDTH = imgs[0].width
-  HEIGHT = imgs.reduce(0) { |a, e| a += e.fith(WIDTH) }
-
-  Image.render(WIDTH, HEIGHT) do 
-    y = 0
-    imgs.each_with_index do |img, i| 
-      h = img.fith(WIDTH)
-      img.draw(0, y, WIDTH, h)
-      y += h
-    end
-  end
-end
-
-class Image
-  def fith(w)
-     (height.to_f / width) * w
-  end
+  imgs = Image.pick_from_library(100)
+  ImageUtil.vertical(imgs)
 end
