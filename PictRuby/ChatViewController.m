c5b6e0c2ba61d55537efb6dd3c7c946924ca9538
ongaeshi <ongaeshi0621@gmail.com>
2016-03-12 14:35:14 +0900

    Format code

--- PictRuby/ChatViewController.m
+++ PictRuby/ChatViewController.m
@@ -15,23 +15,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // 自分の senderId, senderDisplayName を設定
-    self.senderId = @"user1";
-    self.senderDisplayName = @"classmethod";
-    // MessageBubble (背景の吹き出し) を設定
+
+    self.senderId = @"you";
+    self.senderDisplayName = @"You";
+
     JSQMessagesBubbleImageFactory *bubbleFactory = [JSQMessagesBubbleImageFactory new];
     self.incomingBubble = [bubbleFactory  incomingMessagesBubbleImageWithColor:[UIColor jsq_messageBubbleLightGrayColor]];
     self.outgoingBubble = [bubbleFactory  outgoingMessagesBubbleImageWithColor:[UIColor jsq_messageBubbleGreenColor]];
-    // アバター画像を設定
+
     // self.incomingAvatar = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"User2"] diameter:64];
     // self.outgoingAvatar = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"User1"] diameter:64];
-    // メッセージデータの配列を初期化
+
     self.messages = [NSMutableArray array];
+}
+
+- (void)didPressSendButton:(UIButton *)button
+           withMessageText:(NSString *)text
+                  senderId:(NSString *)senderId
+         senderDisplayName:(NSString *)senderDisplayName
+                      date:(NSDate *)date
+{
+    [JSQSystemSoundPlayer jsq_playMessageSentSound];
+
+    JSQMessage *message = [JSQMessage messageWithSenderId:senderId
+                                              displayName:senderDisplayName
+                                                     text:text];
+    [self.messages addObject:message];
+
+    [self finishSendingMessageAnimated:YES];
+
+    // Receive message automatically
+    [self receiveAutoMessage];
 }
 
 - (void)receiveAutoMessage
 {
-    // 1秒後にメッセージを受信する
     [NSTimer scheduledTimerWithTimeInterval:1
                                      target:self
                                    selector:@selector(didFinishMessageTimer:)
@@ -41,44 +59,21 @@
 
 - (void)didFinishMessageTimer:(NSTimer*)timer
 {
-    // 効果音を再生する
     [JSQSystemSoundPlayer jsq_playMessageSentSound];
-    // 新しいメッセージデータを追加する
-    JSQMessage *message = [JSQMessage messageWithSenderId:@"user2"
-                                              displayName:@"underscore"
-                                                     text:@"Hello"];
+
+    JSQMessage *message = [JSQMessage messageWithSenderId:@"ruby"
+                                              displayName:@"Ruby"
+                                                     text:@"こんにちは"];
     [self.messages addObject:message];
-    // メッセージの受信処理を完了する (画面上にメッセージが表示される)
+
     [self finishReceivingMessageAnimated:YES];
 }
 
-// Sendボタンが押下されたときに呼ばれる
-- (void)didPressSendButton:(UIButton *)button
-           withMessageText:(NSString *)text
-                  senderId:(NSString *)senderId
-         senderDisplayName:(NSString *)senderDisplayName
-                      date:(NSDate *)date
-{
-    // 効果音を再生する
-    [JSQSystemSoundPlayer jsq_playMessageSentSound];
-    // 新しいメッセージデータを追加する
-    JSQMessage *message = [JSQMessage messageWithSenderId:senderId
-                                              displayName:senderDisplayName
-                                                     text:text];
-    [self.messages addObject:message];
-    // メッセージの送信処理を完了する (画面上にメッセージが表示される)
-    [self finishSendingMessageAnimated:YES];
-    // 擬似的に自動でメッセージを受信
-    [self receiveAutoMessage];
-}
-
-// アイテムごとに参照するメッセージデータを返す
 - (id<JSQMessageData>)collectionView:(JSQMessagesCollectionView *)collectionView messageDataForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     return [self.messages objectAtIndex:indexPath.item];
 }
 
-// アイテムごとの MessageBubble (背景) を返す
 - (id<JSQMessageBubbleImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView messageBubbleImageDataForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     JSQMessage *message = [self.messages objectAtIndex:indexPath.item];
@@ -88,7 +83,6 @@
     return self.incomingBubble;
 }
 
-// アイテムごとのアバター画像を返す
 - (id<JSQMessageAvatarImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView avatarImageDataForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     JSQMessage *message = [self.messages objectAtIndex:indexPath.item];
@@ -98,7 +92,6 @@
     return self.incomingAvatar;
 }
 
-// アイテムの総数を返す
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
     return self.messages.count;
