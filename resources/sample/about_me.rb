4106fecaa9d0fb5d9cfcbd4717e61ad316e6ca67
ongaeshi <ongaeshi0621@gmail.com>
2016-08-12 13:33:02 +0900

    Refine sample code

---
+++ resources/sample/about_me.rb
@@ -0,0 +1,18 @@
+# # about_me
+#
+# ## Description
+# Self introduction
+
+puts <<EOF
+RubyPico
+http://rubypico.ongaeshi.me/
+
+GitHub
+https://github.com/ongaeshi/RubyPico
+
+Sample Code
+https://github.com/ongaeshi/RubyPicoGems
+
+Twitter
+https://twitter.com/ongaeshi
+EOF
--- resources/sample/lineno.rb
+++ resources/sample/lineno.rb
@@ -1,10 +1,11 @@
-# Please return the text or image in the "def main"
+# # lineno
+#
+# ## Description
+# Put a line no
 
-def main
-  src = Clipboard.get.split("\n")
-  column = src.length.to_s.length
-  
-  (1..src.length).map do |e|
-    puts sprintf("%#{column}d: %s", e, src[e - 1])
-  end
+src = Clipboard.get.split("\n")
+column = src.length.to_s.length
+
+(1..src.length).map do |e|
+  puts sprintf("%#{column}d: %s", e, src[e - 1])
 end
