c9973668f85151460a49c29dcf88fdf6ab777f3b
ongaeshi <ongaeshi0621@gmail.com>
2016-11-20 01:07:06 +0900

    Create link of default tools

--- App/AppViewController.m
+++ App/AppViewController.m
@@ -1,26 +1,39 @@
-#include "AppViewController.h"
+#import "AppViewController.h"
 
-#include "FCFileManager.h"
+#import "FCFileManager.h"
 
 @interface AppViewController ()
 
 @end
 
-@implementation AppViewController
+@implementation AppViewController {
+    NSString* _appDir;
+}
 
-- (id)init
-{
-    NSString *appDir = [[FCFileManager pathForDocumentsDirectory] stringByAppendingPathComponent:@".app"];
+- (id)init {
+    _appDir = [[FCFileManager pathForDocumentsDirectory] stringByAppendingPathComponent:@".app"];
 
-    if (![FCFileManager existsItemAtPath:appDir]) {
-        [FCFileManager createDirectoriesForPath:appDir];
+    if (![FCFileManager existsItemAtPath:_appDir]) {
+        [FCFileManager createDirectoriesForPath:_appDir];
+        
+        // Link default tools
+        NSString *sampleDir = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"sample"];
+        [self createLinkAtPath:[sampleDir stringByAppendingPathComponent:@"irb.rb"]];
     }
 
-    self = [super initWithFileDirectory: appDir
+    self = [super initWithFileDirectory: _appDir
                                   title: @"App"
-                                   edit: false
-        ];
+                                   edit: false];
     return self;
+}
+
+- (void)createLinkAtPath:(NSString*)destPath {
+    NSString *name = [[destPath lastPathComponent] stringByDeletingPathExtension];
+    NSString *path = [_appDir stringByAppendingPathComponent:name];
+
+    [[NSFileManager defaultManager] createSymbolicLinkAtPath:path
+                                         withDestinationPath:destPath
+                                                       error:nil];
 }
 
 @end
