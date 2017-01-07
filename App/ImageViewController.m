6e38a00ba28a370e478f4af5a2e65f91facda8b7
ongaeshi <ongaeshi0621@gmail.com>
2017-01-07 14:57:17 +0900

    Impliment ImageViewController with UIScrollView

--- App/ImageViewController.m
+++ App/ImageViewController.m
@@ -14,6 +14,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    UIScrollView* scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.backgroundColor = [UIColor colorWithRed:0.95 green:0.95 blue:0.95 alpha:1.0];
+
+    UIImage *image = [UIImage imageWithContentsOfFile:_path];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    [scrollView addSubview:imageView];
+
+    scrollView.contentSize = imageView.bounds.size;
+    [self.view addSubview:scrollView];
 }
 
 - (void)didReceiveMemoryWarning {
