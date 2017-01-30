05edb4dcd57afa41544ebf78a1cb81d528fa0994
ongaeshi <ongaeshi0621@gmail.com>
2017-01-31 02:08:05 +0900

    Implement directory view

--- resources/sample/file_server.rb
+++ resources/sample/file_server.rb
@@ -15,7 +15,33 @@ server.http do |r|
 end
 
 server.location "/" do |r|
-  server.file_response r, r.path[1..-1], "text/plain"
+  path = File.join server.config[:document_root], r.path
+
+  if File.directory? path
+    server.set_response_headers "Content-type" => "text/html; charset=utf-8"
+
+    files = Dir.entries(path).map do |e|
+      "    <li><a href=\"#{File.join r.path, e}\">#{e}</a></li>"
+    end
+
+    server.response_body = <<EOS
+<html>
+<head>
+  <title>#{r.path}</title>
+</head>
+<body>
+  <h1>#{r.path}</h1>
+  <ul>
+    #{files.join("\n")}
+  </ul>
+</body>
+</html>
+EOS
+
+    server.create_response
+  else
+    server.file_response r, path, "text/plain"
+  end
 end
 
 puts server.url
